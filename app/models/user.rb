583dc38a32e7808c0b12ef72ef68b7dfe08192c0
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-10-06 23:30:00 +0300

    add association to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable, :validatable,
          :jwt_authenticatable, jwt_revocation_strategy: self
 
+
+  has_many :challenges
+  has_many :solutions
+  has_many :comments
+  has_many :votes
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   # devise :database_authenticatable, :registerable,
